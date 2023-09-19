9585abc865cee270f203e8ab753da7f32e5d18e7
nabezokodaikon <nabezoko.daikon@gmail.com>
2023-09-19 14:23:21 +0900

    Completed

---
+++ IconUtil.cs
@@ -0,0 +1,96 @@
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using WinApi;
+using static WinApi.WinApiMembers;
+
+namespace IconExtract
+{
+    internal static class IconUtil
+    {
+        public static Image? GetSmallIconByFilePath(string filePath)
+        {
+            if (filePath == null)
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            var sh = new WinApiMembers.SHFILEINFOW();
+            var hSuccess = WinApiMembers.SHGetFileInfoW(filePath, 0, ref sh, (uint)Marshal.SizeOf(sh),
+                                                           WinApiMembers.ShellFileInfoFlags.SHGFI_ICON |
+                                                           WinApiMembers.ShellFileInfoFlags.SHGFI_SMALLICON);
+            try
+            {
+                if (!hSuccess.Equals(IntPtr.Zero))
+                {
+                    using (var icon = Icon.FromHandle(sh.hIcon))
+                    {
+                        return icon.ToBitmap();
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            finally
+            {
+                WinApiMembers.DestroyIcon(sh.hIcon);
+            }
+        }
+
+        public static Image? GetExtraLargeIconByFilePath(string filePath)
+        {
+            if (filePath == null)
+            {
+                throw new ArgumentNullException(nameof(filePath));
+            }
+
+            var shinfo = new WinApiMembers.SHFILEINFO();
+            var hSuccess = WinApiMembers.SHGetFileInfo(
+                filePath,
+                0,
+                ref shinfo,
+                (int)Marshal.SizeOf(typeof(WinApiMembers.SHFILEINFO)),
+                (int)WinApiMembers.ShellFileInfoFlags.SHGFI_SYSICONINDEX);
+            if (hSuccess.Equals(IntPtr.Zero))
+            {
+                return null;
+            }
+
+            var result = WinApiMembers.SHGetImageList(
+                SHIL.SHIL_JUMBO,
+                WinApiMembers.IID_IImageList,
+                out IntPtr pimgList);
+
+            try
+            {
+                if (result != WinApiMembers.S_OK)
+                {
+                    return null;
+                }
+
+                var hicon = WinApiMembers.ImageList_GetIcon(pimgList, shinfo.iIcon, 0);
+                if (hicon.Equals(IntPtr.Zero))
+                {
+                    return null;
+                }
+
+                try
+                {
+                    using (var icon = Icon.FromHandle(hicon))
+                    {
+                        return icon.ToBitmap();
+                    }
+                }
+                finally
+                {
+                    WinApiMembers.DestroyIcon(hicon);
+                }
+            }
+            finally
+            {
+                WinApiMembers.ImageList_Destroy(pimgList);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing.Imaging;
+using System.Reflection;
+
+namespace IconExtract
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine(@"
+1: Icon size. ('s' or 'l')
+2: EXE file path.
+");
+                Console.ReadLine();
+                return;
+            }
+
+            var size = args[0];
+            var filePath = args[1];
+
+            var assembly = Assembly.GetExecutingAssembly();
+            if (assembly == null) 
+            {
+                return;
+            }
+
+            var exportDirPath = Path.GetDirectoryName(assembly.Location);
+            if (exportDirPath == null)
+            {
+                return;
+            }
+
+            if (!Directory.Exists(exportDirPath))
+            { 
+                Directory.CreateDirectory(exportDirPath);
+            }
+
+            Console.WriteLine(exportDirPath);
+
+            var exportFileName = string.Format("{0}\\{1}.png", exportDirPath, Path.GetFileNameWithoutExtension(filePath));
+            var exportFilePath = Path.Combine(exportDirPath, exportFileName);
+
+            if (size == "s")
+            {
+                var icon = IconUtil.GetSmallIconByFilePath(filePath);
+                if (icon != null)
+                {
+                    using (var fs = new FileStream(exportFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+                    {
+                        icon.Save(fs, ImageFormat.Png);
+                    }
+                }
+            }
+            else if (size == "l")
+            {
+                var icon = IconUtil.GetExtraLargeIconByFilePath(filePath);
+                if (icon != null)
+                {
+                    using (var fs = new FileStream(exportFilePath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None))
+                    {
+                        icon.Save(fs, ImageFormat.Png);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(@"
+1: Icon size. ('s' or 'l')
+2: EXE file path.
+");
+                Console.ReadLine();
+            }
+        }
+    }
+}
